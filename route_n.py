e8e9067c2a6a1f733a890fc792c806f52b6720f3
zline <zh384144429@sina.com>
2019-10-29 22:13:55 +0800

    20191029

--- route_n.py
+++ route_n.py
@@ -7,3 +7,13 @@ route_result = os.popen('route -n').read()
 ipv4_gw = re.findall('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',route_result)[1]
 
 print('网关为:{0}'.format(ipv4_gw))
+
+# ip_add = re.findall('\d*\.\d*\.\d*\.\d',route_result)[1]
+# # print(ip_add)
+# flags = re.findall('UG',route_result)[0]
+# # print(flags)
+# need = [ip_add,flags]
+# if need in route_result:
+#     print('网关为：'+ ipv4_gw )
+
+
---
+++ tcp80.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+
+from http.server import HTTPServer, CGIHTTPRequestHandler
+port = 80
+httpd = HTTPServer(('',port), CGIHTTPRequestHandler)
+print('Starting simple httpd on port: ' + str(httpd.server_port))
+httpd.serve_forever()

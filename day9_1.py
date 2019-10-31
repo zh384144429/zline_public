41d49c2a1c8cc01e5d7adad4ab54208866c3abd5
zline <zh384144429@sina.com>
2019-10-31 22:15:18 +0800

    20191029

--- day9_1.py
+++ day9_1.py
@@ -12,7 +12,8 @@ def qytang_ssh(ip,username,password,port=22,cmd='ls'):
     x = stdout.read().decode()
     return x
 if __name__ == '__main__':
-    print(qytang_ssh('10.255.8.130','root','11111111',cmd='nmcli d show ens33'))
+    print(qytang_ssh('10.255.255.128', 'root', 'a.1'))
+    print(qytang_ssh('10.255.255.128','root','a.1',cmd='pwd'))
     # print(qytang_ssh('10.255.8.130', 'root', '11111111',cmd='nmcli'))
 
 # ssh = paramiko.SSHClient()
